3e02b31fcfbac60a61b4ad69e20ecdaf7c7836a9
arthur-gal-pin <arthurrgspinto@gmail.com>
2026-09-17 06:12:40 -0300

    :sparkles: feat(api): Refatorização da model Amostra
    Adicionando  campos e excluindo campos que não estão mais presentes devido  a tualização nas regras de negócio, que levou à uma alteração no banco de dados, cuja nova versão foi anexada na pasta docs.

---
+++ backend/src/api/enum/amostras/classificacaoAmostra.enum.ts
@@ -0,0 +1,13 @@
+export enum enumClassificacaoAmostra{
+    filtro = 'filtro',
+    peca = 'peça'
+}
+
+export enum enumSubclassificacaoAmostra {
+    filtroParticula = 'Filtro para Partículas',
+    filtroQuimico = 'Filtro Quimico',
+    filtroCombinado = 'Filtro Combinado',
+    pecaFacial = 'Peça Facial Inteira',
+    pecaQuarto = 'Peça Facial 1/4',
+    pecaSemi = 'Peça SemiFacial'
+}
